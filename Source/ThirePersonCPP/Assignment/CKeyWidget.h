0cc2d54a29e8a4f06fad0ad3459227c9feff1c7b
jinyeongking <119106497+jinyeongking@users.noreply.github.com>
2024-11-15 18:08:14 +0900

    Update CKeyWidget.h

--- Source/ThirePersonCPP/Assignment/CKeyWidget.h
+++ Source/ThirePersonCPP/Assignment/CKeyWidget.h
@@ -1,17 +1,1 @@
-#pragma once
 
-#include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "CKeyWidget.generated.h"
-
-class UImage;
-
-UCLASS()
-class THIREPERSONCPP_API UCKeyWidget : public UUserWidget
-{
-	GENERATED_BODY()
-
-public:
-	UFUNCTION(BlueprintImplementableEvent)
-	void ActiveKeyIcon(int32 index);
-};
